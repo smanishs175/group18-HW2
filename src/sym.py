ae291a8d815163618cd9fe2f5804c0a83d139a40
Shruti Verma~ <sverma5@ncsu.edu>
2022-09-05 16:13:06 -0400

    Added init and mid method to sym.py

---
+++ src/sym.py
@@ -0,0 +1,32 @@
+class Sym:
+
+    def __init__(self, c, s):
+        self.n = 0
+        self.at = 0
+        self.name = ""
+        self._has = {}
+        if c:
+            self.at = c
+        if s:
+            self.name = s
+        return (self.n, self.at, self.name, self._has)
+
+
+    def mid(self, col, most, mode):
+        most = -1
+        for k, v in self._has.items():
+            if v > most:
+                mode, most = k, v
+        return mode
+
+
+
+
+
+
+
+
+
+
+
+
