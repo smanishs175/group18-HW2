27f6ad8d0aeeec9087a8ea8adfcf2fd8d0a0975d
Palash <palash10129@gmail.com>
2022-09-10 19:39:37 -0400

    Added comments

--- src/cols.py
+++ src/cols.py
@@ -10,12 +10,17 @@ class Cols:
         self.y={}
 
         for c,s in names.items():
+            
             start_Caps = re.search(r'^[A-Z]*',s)
+            ## if starts with Uppercase
             if start_Caps and 
             col = utils.push(self.all,
                              s.find)
+            ## ends with :
             ends_with_colon = re.search(r':$',s)
+            ## ends with !
             ends_with_exc = re.search(r'!$',s)
+            ## contains ! or + or -
             signs_present = re.search(r'[!+-]',s)
             if not ends_with_colon:
                 if signs_present:
