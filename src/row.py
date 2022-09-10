bee180affd30a6188fe009b2a60127e1ecc80b91
Manish Shinde <smanishs175@gmail.com>
2022-09-10 14:06:27 -0400

    Updated row.py

--- src/row.py
+++ src/row.py
@@ -1,6 +1,7 @@
-class Row:
-    def __init__(self, t):
-        self.cells = t  # one record
-        self.cooked = t.copy()  # used if we discretize data
-        self.isEvaled = False  # true if y-values evaluated
+from utils import copy
 
+class Row:
+  def __init__(self, t):
+    self.cell = t
+    self.cooked = copy(t)
+    self.isEvaled = False
