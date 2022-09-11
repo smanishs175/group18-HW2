cd42597dee52e1f4aaa9af79907d42d46c79bb3a
Shruti Verma~ <sverma5@ncsu.edu>
2022-09-10 20:09:19 -0400

    Refactored code in cols.py

--- src/cols.py
+++ src/cols.py
@@ -1,35 +1,41 @@
 import utils
+import num
+import sym
 import re
 class Cols:
 
     def __init__(self,names):
         self.names=names
-        self.all= {}
+        self.all= []
         self.klass=None
-        self.x= {}
-        self.y={}
+        self.x= []
+        self.y=[]
 
         for c,s in names.items():
-            
+
             start_Caps = re.search(r'^[A-Z]*',s)
             ## if starts with Uppercase
-            if start_Caps and 
-            col = utils.push(self.all,
-                             s.find)
+            if start_Caps:
+                obj=num.Num(c,s)
+            else:
+                obj = sym.Sym(c,s)
+
+            col = utils.push(self.all,obj)
+
             ## ends with :
             ends_with_colon = re.search(r':$',s)
             ## ends with !
             ends_with_exc = re.search(r'!$',s)
             ## contains ! or + or -
             signs_present = re.search(r'[!+-]',s)
+
             if not ends_with_colon:
                 if signs_present:
                     utils.push(self.x,col)
                 else:
                     utils.push(self.y, col)
-                
+
                 if ends_with_exc:
                     self.klass = col
-                    
-            
-        
+
+
